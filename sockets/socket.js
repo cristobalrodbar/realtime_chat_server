6aef5aab9fe2b12c944f916949329b3a3f2eb09d
Cristóbal Rodríguez <cristobalrodbar@gmail.com>
2023-07-06 17:56:14 -0600

    retornar usuarios

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,22 +1,27 @@
 const { io } = require('../index');
 const { comprobarJWT } = require('../helpers/jwt');
-
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
 
     //cliente con JWT?
-    //console.log(client.handshake.headers['x-token']);
     const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
-    //console.log(valido, uid);
-    if (!valido) {
-        return client.disconnect();
-    }
+    console.log(valido, uid);
+    console.log(client.handshake.headers['x-token']);
+
+    //verificar autenticación
+    if (!valido) { return client.disconnect(); }
+    
+    //cliente autenticado
+    usuarioConectado(uid);
+
     console.log('cliente autenticado');
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
     client.on('mensaje', ( payload ) => {
